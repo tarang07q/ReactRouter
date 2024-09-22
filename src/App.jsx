1b66e289f4a72966d40e89c34c2ccb91a466c0da
tarang07q <123387466+tarang07q@users.noreply.github.com>
2024-09-22 16:46:06 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './components/Home/Home.jsx'
+import Layout from './Layout.jsx'
+import About from './components/About/About.jsx'
+import Contact from './components/Contact/Contact.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />, // Use JSX syntax here
+    children: [
+      {
+        path: "",
+        element: <Home />
+      },
+      {
+        path: "about",
+        element: <About />
+      },
+      {
+        path: "contact",
+        element: <Contact /> // Assuming you want this route too
+      },
+    ]
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
